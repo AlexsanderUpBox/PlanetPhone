f7e60aac47a8986a5e93ffb0b1e2c087a93753ed
AlexsanderUpBox <156192268+AlexsanderUpBox@users.noreply.github.com>
2024-02-02 16:09:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    var hamburger = document.querySelector('.hamburger');
+    var menu = document.querySelector('.menu');
+    var menuClose = document.querySelector('.menu-close');
+
+    hamburger.addEventListener('click', function() {
+        menu.classList.add('is-active');
+    })
+
+    menuClose.addEventListener('click', function() {
+        menu.classList.remove('is-active');
+    })
+
+});
